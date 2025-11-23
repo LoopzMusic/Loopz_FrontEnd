cf45a5c2285535cedfc75eda1e33c819868ad9f3
lauraabress <laurabrssn@gmail.com>
2025-11-23 16:36:06 -0300

    Feat = Função Add Favoritos Funcionando

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterLink } from "@angular/router";
 import { Produto } from '../../shared/models/Produto';
 
@@ -10,19 +10,47 @@ import { Produto } from '../../shared/models/Produto';
 })
 export class Card {
 
-   @Input({ required: true }) produto: Produto = new Produto();
+  @Input({ required: true }) produto: Produto = new Produto();
+  @ViewChild('toastFavorito') toastFavorito!: ElementRef;
+
+  favorito = false;
 
   constructor(private router: Router) {}
 
-  adicionarFavorito() {
-    // pegar favoritos existentes
+  ngOnInit() {
     const favoritos = JSON.parse(localStorage.getItem('favoritos') || '[]');
+    this.favorito = favoritos.some((p: any) => p.cdProduto === this.produto.cdProduto);
+  }
 
-    // adicionar o novo produto
-    favoritos.push(this.produto);
+  adicionarFavorito() {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos') || '[]');
 
-    // salvar de volta
-    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    if (this.favorito) {
+      favoritos = favoritos.filter((p: any) => p.cdProduto !== this.produto.cdProduto);
+      localStorage.setItem('favoritos', JSON.stringify(favoritos));
+
+      this.favorito = false;
+
+      if (this.router.url.includes('favoritos')) {
+        this.router.navigate(['/produtos']);
+      }
+
+      this.showToast("Removido dos favoritos!");
+    } else {
+      favoritos.push(this.produto);
+      localStorage.setItem('favoritos', JSON.stringify(favoritos));
+
+      this.favorito = true;
+      this.showToast("Adicionado aos favoritos!");
+    }
+  }
+
+  showToast(msg: string) {
+    this.toastFavorito.nativeElement.querySelector('.toast-body').textContent = msg;
+
+    // @ts-ignore
+    const toast = new bootstrap.Toast(this.toastFavorito.nativeElement);
+    toast.show();
   }
 
 }
--- src/app/pages/favoritos/favoritos.ts
+++ src/app/pages/favoritos/favoritos.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Card } from "../../components/card/card";
 
 @Component({
   selector: 'app-favoritos',
-  imports: [],
+  imports: [Card],
   templateUrl: './favoritos.html',
   styleUrl: './favoritos.scss',
 })
 export class Favoritos {
+  favoritos = JSON.parse(localStorage.getItem('favoritos') || '[]');
 
 }
