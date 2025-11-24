783e86c23eb27269c34de52a55aa1a949c5caaa7
lauraabress <laurabrssn@gmail.com>
2025-11-24 09:25:05 -0300

    Feat = Início da criação da tela de login

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import { Usuario } from '../../shared/models/Usuario';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
   templateUrl: './perfil.html',
-  styleUrl: './perfil.scss',
 })
-export class Perfil {
+export class Perfil implements OnInit {
+  usuario!: Usuario | null;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.usuario = this.authService.getUsuarioLogado();
+
+    if (!this.usuario) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  editarPerfil() {
+    this.router.navigate(['/editar-perfil']);
+  }
 }
