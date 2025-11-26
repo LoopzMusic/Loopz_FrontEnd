5be69bbb560794aa9bfe82e2855bab3f774d6049
lauraabress <laurabrssn@gmail.com>
2025-11-26 15:04:07 -0300

    Feat = Add cards de 'Produtos Relacionados'

--- src/app/pages/mais-detalhes/mais-detalhes.ts
+++ src/app/pages/mais-detalhes/mais-detalhes.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CardAvaliacao } from '../../components/card-avaliacao/card-avaliacao';
 import { ProdutoService } from '../../services/produto-service';
 import { Produto } from '../../shared/models/Produto';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-mais-detalhes',
-  imports: [RouterLink, CardAvaliacao],
+  imports: [RouterLink, CardAvaliacao, Card],
   templateUrl: './mais-detalhes.html',
   styleUrl: './mais-detalhes.scss',
 })
@@ -14,6 +15,7 @@ export class MaisDetalhes implements OnInit {
   private produtoService = inject(ProdutoService);
   private route = inject(ActivatedRoute);
 
+  protected produtos: Produto[] = [];
   produto: Produto = new Produto();
   // avaliacoes: Avaliacao[] = [];
 
@@ -26,8 +28,10 @@ export class MaisDetalhes implements OnInit {
       this.produto = response;
     });
 
-    // this.produtoService.buscarAvaliacoesPorProdutoId(id).subscribe(response => {
-    //   this.avaliacoes = response;
-    // });
+    this.produtoService.listarProdutos().subscribe((response) => (this.produtos = response));
   }
+
+  // this.produtoService.buscarAvaliacoesPorProdutoId(id).subscribe(response => {
+  //   this.avaliacoes = response;
+  // });
 }
