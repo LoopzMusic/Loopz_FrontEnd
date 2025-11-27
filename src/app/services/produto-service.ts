76aae600293171adc1c97d6e781a75612ee6f831
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-26 23:18:24 -0300

    Feat: EndPoint de recomendação integrado e funcional

--- src/app/services/produto-service.ts
+++ src/app/services/produto-service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Produto } from '../shared/models/Produto';
+import { Observable, forkJoin, of } from 'rxjs';
+import { map, switchMap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProdutoService {
   private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8085/produto';
+  private apiUrlRecomendacao = 'http://localhost:8085/recomendacao';
 
   listarProdutos() {
     return this.http.get<Produto[]>('http://localhost:8085/produto/listar/todos');
@@ -28,5 +32,39 @@ export class ProdutoService {
   //cadastrar estoque do produto
   criarEstoque(body: any) {
     return this.http.post<any>('http://localhost:8085/estoque/criar', body);
+  }
+
+  
+  buscarProdutosRecomendados(cdProduto: number): Observable<Produto[]> {
+    return this.http.get<any[]>(`${this.apiUrlRecomendacao}/${cdProduto}/recomendados`).pipe(
+      switchMap(recomendados => {
+        
+        if (!recomendados || recomendados.length === 0) {
+          return of([]);
+        }
+
+        
+        const ids = recomendados.map(r => r.cdProduto);
+
+        
+        const requests = ids.map(cdProduto => 
+          this.buscarProdutoPorId(cdProduto).pipe(
+            catchError(error => {
+              console.error(`Erro ao buscar produto ${cdProduto}:`, error);
+              return of(null);
+            })
+          )
+        );
+
+        
+        return forkJoin(requests).pipe(
+          map(produtos => produtos.filter(p => p !== null) as Produto[])
+        );
+      }),
+      catchError(error => {
+        console.error('Erro ao buscar produtos recomendados:', error);
+        return of([]);
+      })
+    );
   }
 }
