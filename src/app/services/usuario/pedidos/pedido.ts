05ec725b06941efb17d82159c399b519cb21d95e
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-29 03:17:15 -0300

    Feat: Adicionado endPoint de admin para trazer os pedidos como auditoria.

--- src/app/services/usuario/pedidos/pedido.ts
+++ src/app/services/usuario/pedidos/pedido.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PedidoResumo } from '../../../shared/models/usuario/PedidosUsuarios';
+import { PedidoResumo, PedidoResumoAdminTodos } from '../../../shared/models/usuario/PedidosUsuarios';
 import { AuthService } from '../../auth-service';
 
 @Injectable({
@@ -10,6 +10,7 @@ import { AuthService } from '../../auth-service';
 })
 export class PedidoService {
   private apiUrl = 'http://localhost:8085/pedido';
+  private apiUrlAdmin = 'http://localhost:8085/admin/auditoria/pedidos';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
@@ -21,4 +22,13 @@ export class PedidoService {
 
     return this.http.get<PedidoResumo[]>(`${this.apiUrl}/meus`, { headers });
   }
+
+  listarTodosPedidosAdmin(): Observable<PedidoResumoAdminTodos[]> {
+    return this.http.get<PedidoResumoAdminTodos[]>(this.apiUrlAdmin);
+  }
+
+  atualizarStatusPedido(cdPedido: number, status: string): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/atualizar/${cdPedido}/status`, { status });
+  }
+  
 }
